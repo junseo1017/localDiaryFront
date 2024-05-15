f0955f2b71d13a280ea8e64468441d01ead0d4fa
leeJS <dlwnstjzld@gmail.com>
2024-05-15 14:01:12 +0900

    update request url, /user/main

--- src/apis/mainPageLoading.tsx
+++ src/apis/mainPageLoading.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const getMainPage: () => Promise<string> = async () => {
   try {
-    const res = await axios.get(`/main`);
+    const res = await axios.get(`/user/main`);
     return res.data;
   } catch (error) {
     throw new Error("通信に失敗しました。");
